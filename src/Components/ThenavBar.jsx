104efe60a20a3cb97bbab550c893752c674af3e7
abdo2008new <tamine.abderrahman@gmail.com>
2025-09-12 01:28:42 +0100

    update portfolio

--- src/Components/ThenavBar.jsx
+++ src/Components/ThenavBar.jsx
@@ -51,6 +51,24 @@ if (isMenuOpen && isMobile) {
     setIsMenuOpen(false);
     }
 };
+
+useEffect(() => {
+let scrollY = 0;
+if (isMenuOpen) {
+    scrollY = window.scrollY;
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.width = "100%";
+    } else {
+    const y = document.body.style.top;
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.width = "";
+    window.scrollTo(0, parseInt(y || "0") * -1);
+}
+}, [isMenuOpen]);
+
+
 window.addEventListener("resize", handleResize);
 return () => window.removeEventListener("resize", handleResize);
 }, []);
