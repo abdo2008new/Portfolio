d1d7663be89a0d4653c9938626f1b80112d1fbb8
abdo2008new <tamine.abderrahman@gmail.com>
2025-09-12 23:52:09 +0100

    update portfolio

--- src/Components/HomeSection.jsx
+++ src/Components/HomeSection.jsx
@@ -5,10 +5,10 @@ export const HomeSection = () => {
         <section id="home" className="relative min-h-screen flex flex-col items-center justify-center px-4">
             <div className="container max-w-4xl mx-auto text-center z-10">
         <div className="space-y-6">
-<h1 className="text-4xl font-bold  md:text-6xl tracking-tight">
-    <span className="opacity-0 animate-fade-in">Hi, I'm </span>
-    <span className="text-primary opacity-0 animate-fade-in-delay-1">Abderrahmane</span>
-    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">Tamine</span>
+<h1 className="text-3xl sm:text-4xl md:text-6xl font-bold tracking-tight">
+    <span className="opacity-0 animate-fade-in block sm:inline">Hi, I'm </span>
+    <span className="text-primary opacity-0 animate-fade-in-delay-1 block sm:inline">Abderrahmane</span>
+    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2 block sm:inline">Tamine</span>
 </h1>
 <p className="text-lg md:text-xl text-muted-foreground mx-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
     I’m a developer who enjoys turning ideas into functional projects
